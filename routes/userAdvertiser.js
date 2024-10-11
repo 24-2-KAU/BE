f9bda5757ccc00d31cb079d70272c4c027682094
yoonjiwon1102 <ueong1016@naver.com>
2024-10-12 02:37:17 +0900

    필요없는 코드 삭제

--- routes/userAdvertiser.js
+++ routes/userAdvertiser.js
@@ -11,21 +11,16 @@ const connection  = require('../database/connect/mysql');
 router.post("/api/users/signup", async (req, res) => {
   const { ad_id, password, phone, email } = req.body;
 
-  if (!ad_id || !email || !password || !phone) {
-    return res.status(400).send('모든 필드를 입력해 주세요.');
-  }
   
   // DB에 데이터 저장
   connection.query('SELECT * FROM user_advertiser WHERE email = ?', [email], (err, results) => {
     if (err) {
       console.error('DB 조회 오류:', err);
-      return res.status(500).send('DB 조회 중 오류 발생');
     }
 
     if (results.length > 0) {
       // 이미 회원가입된 사용자
       console.log('이미 회원가입된 사용자:', email);
-      return res.status(400).send('이미 가입된 이메일입니다.');
     } else {
       // 새 사용자 등록
       connection.query(
@@ -34,10 +29,8 @@ router.post("/api/users/signup", async (req, res) => {
         (err, result) => {
           if (err) {
             console.error('회원가입 실패:', err);
-            return res.status(500).send('회원가입 중 오류 발생');
           }
           console.log('회원가입 성공:', email);
-          res.status(201).send('회원가입 성공');
         }
       );
     }
@@ -49,14 +42,9 @@ router.post("/api/users/signup", async (req, res) => {
 router.post('/api/users/login', async (req, res) => {
   const {ad_id,password} = req.body;
 
-  if (!ad_id || !password) {
-    return res.status(400).send('아이디와 비밀번호를 입력하세요!');
-  }
-
   connection.query('SELECT * FROM mydb.user_advertiser WHERE ad_id = ?', [ad_id], (err, results) => {
     if (err) {
         console.error('DB 조회 오류:', err);
-        return res.status(500).send('DB 조회 중 오류 발생');
     }
 
     if (results.length > 0) {
