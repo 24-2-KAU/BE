b7243764c80c37d586c0af6894eea16c4d9c7fac
YoonJiWon1102 <ueong1016@naver.com>
2024-12-07 00:42:45 +0900

    app.js 오타수정

--- app.js
+++ app.js
@@ -42,7 +42,7 @@ app.use(express.static(path.join(__dirname, '../')));
 const server = http.createServer(app);
 const io = new Server(server, {
   cors: {
-    origin: ['http://localhost:4000, https://ad-influencer.com','http://127.0.0.1:5500', 'http://localhost:3000'], // 필요한 도메인 추가
+    origin: ['http://localhost:4000', 'https://ad-influencer.com','http://127.0.0.1:5500', 'http://localhost:3000'], // 필요한 도메인 추가
     methods: ['GET', 'POST'],
     credentials: true
   }
